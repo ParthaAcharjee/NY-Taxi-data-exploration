6593d0b6d10593fd22adadbb8465cce709ccf35c
ParthaAchrajee <partha.rlc@gmail.com>
2017-07-20 19:58:49 -0500

    First commit

--- nyTaxiDataExploration.py
+++ nyTaxiDataExploration.py
@@ -1,13 +1,15 @@
+# This NYC taxi data is avilable at kaggle
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-train = pd.read_csv('C:\Users\ppa3551\Documents\GitHub\NY Taxi data exploration\\train.csv')
-test = pd.read_csv('C:\Users\ppa3551\Documents\GitHub\NY Taxi data exploration\\test.csv')
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
 
-print "Training Samples: ", train.shape[0]
-print "Testing Samples: ", test.shape[0]
+print("Training Samples: ", train.shape[0])
+print("Testing Samples: ", test.shape[0])
 
 
 # Let's look at the train data first
